18abb6b470710a5399ee5c205c3a1f9bab4e9bbf
DhanushSpm <dhanusha272004@gmail.com>
2026-01-10 07:03:32 +0530

    Organize string practice files into 'String Concepts' folder

---
+++ String Concepts/StringFunction.py
@@ -0,0 +1,30 @@
+""" String Concatination """
+# Joining of two or more string is called String Concatenation we use + operator for concatenation of two strings
+
+name1 = 'Dhanush'
+name2 = ' Aranganathan'
+print(name1+name2)
+
+""" String Comparison """
+# String Comparison can be done using the relational operators like <>, ==
+
+msg1 = 'aaa'
+msg2 = 'aaa'
+print(msg1 == msg2)
+
+msg3 = 'aa'
+msg4 = 'bb'
+print(msg3>msg4)
+
+""" String Repeatition """
+# We can repeat the string using * operator
+
+msg = 'Welcome'
+print(msg *3)
+
+""" MemberShip Test """
+# The MemberShip of Particular character is determined using the keyword
+
+name = 'Dhanush'
+print('l' in name)
+
