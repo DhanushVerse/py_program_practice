46d238a2b37935034ce3e05a37b55be6372f126a
DhanushSpm <dhanusha272004@gmail.com>
2026-01-11 07:56:24 +0530

    Organize Python String Methods by category - Case Convertion Methods

---
+++ String_Concepts/String_Methods/Case_Convertion_Methods.py
@@ -0,0 +1,25 @@
+# 1.Upper
+# The upper() method converts all characters in a string to uppercase.
+msg = 'learning python is very easy'
+result = msg.upper()
+print(result)
+
+# 2.Lower
+# The lower() method converts all characters in a string to lowercase.
+msg = 'LEARNING PYTHON IS VERY EASY'
+result = msg.lower()
+print(result)
+
+# 3.title
+# The title() method converts the first character of each word to upper case.
+msg = 'learning python is very easy'
+result = msg.title()
+print(result)
+
+# 4.Capitalize
+# The capitalize() method converts the first character of a string to upper case and the rest to lower case.
+msg = 'learning python is very easy'
+result = msg.capitalize()
+print(result)
+
+
