72bb3c97d50d0a3d80ca5a9f959a032992c505e4
DhanushSpm <dhanusha272004@gmail.com>
2026-03-13 11:48:49 +0530

    Argument in the Decorated function

--- Daily_Practice/Decorators.py
+++ Daily_Practice/Decorators.py
@@ -1,5 +1,6 @@
 """ Decorators """
-# Decorators is a function that Wraps another function to add extra functionality without changing the Original function
+# Decorators is a function that Wraps another function to add extra functionality without changing the Original function.
+# A Decorator can be called multiple times.Just place the Decorator above the function to Decorate.
 
 # Example:
 
@@ -20,3 +21,26 @@ def age():
 
 introduce()
 age()
+
+""" Arguments in the Decorated Function """
+# Functions that require arguments can also be decorated, just make sure you pass the arguments to the wrapper function.
+
+# Example:
+
+def check_access(password):
+    def decorator(func):
+        def wrapper():
+            if password == "dhanush123":
+                return func()
+            else:
+                print("Wrong Password Access Denied❌.")
+        return wrapper
+    return decorator
+
+# we want to use decorater here
+pswd = input('Enter the password:')
+@check_access(pswd)
+def open_locker():
+    print("Locker opened!✅")
+
+open_locker()
