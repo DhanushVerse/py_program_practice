4a4b127cf8b8bddc37b0bcd3744038f168a0be72
DhanushSpm <dhanusha272004@gmail.com>
2026-03-21 12:31:03 +0530

    New file created : lambda.py

---
+++ Daily_Practice/lambda.py
@@ -0,0 +1,14 @@
+""" Lambda function """
+# A Lambda function is a small anonymous function
+# lambda function can take any number of arguments with only one expression
+
+""" Syntax """
+# lambda arguments : expression
+#   -> The expression is executed and the result is returned.
+
+# Example:
+add = lambda a : a + 10
+print(add(2))
+
+mul = lambda a,b : a * b
+print(mul(2,5))
