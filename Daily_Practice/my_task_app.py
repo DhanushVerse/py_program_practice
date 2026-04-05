82f9106d23303a4f354f777fe202591da03ce463
DhanushSpm <dhanusha272004@gmail.com>
2026-04-05 22:12:42 +0530

    build my task app

---
+++ Daily_Practice/my_task_app.py
@@ -0,0 +1,19 @@
+# My task app
+def my_task_app():
+   all_tasklist = []
+   while True:
+      task = input('\nEnter the task (Type "exit" to close): ') 
+      if task.lower() == 'exit':
+         break
+      print('1. Completed✅\n2. Partial Completed🤞\n3. Not Completed❌')
+      status = input('Enter the status of your task:')
+      status_check = {'1': 'Completed✅','2': 'Parital Completed','3': 'Not Completed❌','4': 'Exit😙'}
+      current_status = status_check.get(status,"unknown")
+      all_tasklist.append(f'{task} - {current_status}')
+   return all_tasklist
+   
+task_app = my_task_app()
+print('\n The Task list are:')
+for t in task_app:
+   print(t)
+gi
